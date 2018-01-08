8e2c6ddad9d642216505c1ebb1875daa10e35121
Diwakar Mishra <diwakarmishra.himt@gmail.com>
2018-01-08 18:08:19 +0800

    fixed notes createdAt field type and default value

--- notes/models.py
+++ notes/models.py
@@ -1,5 +1,4 @@
 import datetime
-
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -9,5 +8,5 @@ class Note(models.Model):
     type = models.CharField(max_length=20)
     title = models.CharField(max_length=100)
     text = models.TextField(blank=True, null=True)
-    createdAt = models.DateTimeField(default=datetime.datetime, blank=True)
+    createdAt = models.DateTimeField(default=datetime.datetime.now(), blank=True)
     modifiedAt = models.DateTimeField(blank=True, null=True)
